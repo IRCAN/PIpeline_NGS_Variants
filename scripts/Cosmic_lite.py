6fea766486780ac19a22305e44c0498206ae5285
Ludovic Kosthowa <ludovic.kosthowa@gmail.com>
2016-05-04 16:42:38 +0200

    Creation de la DB Cosmic verison allégèe

--- scripts/Cosmic_lite.py
+++ scripts/Cosmic_lite.py
@@ -11,9 +11,9 @@ Info: Creation en cours, script peut etre modifie a tout moment.
 File = "../Data/Cosmic/CosmicCompleteExport.tsv"
 contentFile = open(File,"r")
 cosmic =  contentFile.readlines()
+contentFile.close()
 cosmicIdList = set()
-cosmicDbLite = []
-f_out = "../Data/Cosmic/Cosmic_lite.txt"
+f_out = "../Data/Cosmic/Cosmic_lite_2.txt"
 File_out = open(f_out,'w')
 print("Lecture de la base de données COSMIC...")
 print("Création de Cosmic_lite: merci de patienter...")
@@ -21,5 +21,9 @@ for cosmicLigne in cosmic:
 	cosmicLigneSplit = cosmicLigne.split("\t")
 	if cosmicLigneSplit[16] not in cosmicIdList:
 		cosmicIdList.add(cosmicLigneSplit[16])
-		File_out.write(cosmicLigne)
+		mutation_AA = cosmicLigneSplit[18].replace("\n","")
+		ligne = cosmicLigneSplit[0]+"\t"+cosmicLigneSplit[1]+"\t"+cosmicLigneSplit[3]+"\t"+cosmicLigneSplit[7]+"\t"+cosmicLigneSplit[16]+"\t"+cosmicLigneSplit[17]+"\t"+cosmicLigneSplit[18]+"\n"
+		File_out.write(ligne)
+
+File_out.close()
 print("Création terminée !")
