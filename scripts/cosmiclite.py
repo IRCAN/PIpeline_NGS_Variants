cded049507e732f6ac7dce0c4cbd6d59867c2af8
TessierFlorent <tessier.flrnt@gmail.com>
2016-06-16 14:59:06 +0200

    Update cosmiclite.py

--- scripts/cosmiclite.py
+++ scripts/cosmiclite.py
@@ -14,8 +14,8 @@ class CosmicLite:
 		cosmicIdList = set()
 		f_out = "../System/Cosmic/Cosmic_lite.txt"
 		File_out = open(f_out,'w')
-		print("Lecture de la base de données COSMIC...")
-		print("Création de Cosmic_lite: merci de patienter...")
+		print("Lecture de la base de donnees COSMIC...")
+		print("Creation de Cosmic_lite: merci de patienter...")
 		for cosmicLigne in cosmic:
 			cosmicLigneSplit = cosmicLigne.split("\t")
 			if cosmicLigneSplit[16] not in cosmicIdList:
@@ -24,6 +24,6 @@ class CosmicLite:
 				ligne = cosmicLigneSplit[0]+"\t"+cosmicLigneSplit[1]+"\t"+cosmicLigneSplit[3]+"\t"+cosmicLigneSplit[7]+"\t"+cosmicLigneSplit[16]+"\t"+cosmicLigneSplit[17]+"\t"+cosmicLigneSplit[18]+"\n"
 				File_out.write(ligne)
 		File_out.close()
-		print("Création terminée !")
+		print("Creation terminee !")
 
 
