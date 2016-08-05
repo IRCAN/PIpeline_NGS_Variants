57becee602bf38d68fa53c54e7e4c2c6a83fb57d
Ludovic Kosthowa <ludovic.kosthowa@gmail.com>
2016-08-05 11:35:01 +0200

    Adaptation largeur colonnes

--- scripts/makeReport.py
+++ scripts/makeReport.py
@@ -140,10 +140,13 @@ class MakeReport:
 		report.write("Définitions:\n")
 		report.write("SIFT : Prediction de l'impact d'une substitution sur la fonction de la protéine en se basant sur le degré de conservation des acides aminés\n")
 		report.write("Polyphen : Prédiction de l'impact d'une substitution sur la structure et la fonction des protéines en utilisant des informations de séquences et de structure\n")
-		report.write("HGVSc: the HGVS (Human Genome Variation Society) coding sequence name\n")
-		report.write("HGVSp: the HGVS protein sequence name\n")
+		report.write("HGVSc: le nom de la séquence codante dans HGVS (Human Genome Variation Society)\n")
+		report.write("HGVSp: le nom de la séquence protéique dans HGVS (Human Genome Variation Society)\n")
 		report.write("Profondeur: La profondeur de lecture est le nombre de lectures («reads») indépendantes d'une base par le séquenceur\n")
-		report.write("Couverture: La couverture de séquence est le pourcentage de bases couvertes par rapport au nombre total de bases de la région d'intérêt (pour une profondeur de lecture donnée)\n")
+		report.write("Couverture: La couverture (en pourcentage) est le nombre de bases couvertes par rapport au nombre total de bases de la région d'intérêt (pour une profondeur de lecture donnée)\n")
+		report.write("maf: fréquence de l'allèle minoritaire\n")
+
+
 		report.close()
 
 	def informations(self,REPERTORYVCF,i,RESULTDIR,pathREPERTORYVCF):
@@ -174,18 +177,18 @@ class MakeReport:
 		#Pour le sinformations:
 		ws.column_dimensions["A"].width = 10.0
 		ws.column_dimensions["B"].width =12.0		
-		ws.column_dimensions["C"].width = 15.0
-		ws.column_dimensions["D"].width =12.0		
+		ws.column_dimensions["C"].width = 14.0
+		ws.column_dimensions["D"].width =10.0		
 		ws.column_dimensions["E"].width =12.0		
-		ws.column_dimensions["F"].width =10.0
+		ws.column_dimensions["F"].width =12.0
 		ws.column_dimensions["G"].width = 10.0
-		ws.column_dimensions["H"].width = 10.0
+		ws.column_dimensions["H"].width = 12.0
 		ws.column_dimensions["I"].width = 12.0
 		ws.column_dimensions["J"].width = 12.0
 		ws.column_dimensions["K"].width = 10.0
-		ws.column_dimensions["L"].width =6.0
-		ws.column_dimensions["M"].width = 6.0
-		ws.column_dimensions["N"].width = 6.0
+		ws.column_dimensions["L"].width =7.0
+		ws.column_dimensions["M"].width = 7.0
+		ws.column_dimensions["N"].width = 7.0
 				#Titre du fichier
 		if i[-3:]=="vcf":
 			i=i[:-4]
@@ -199,6 +202,7 @@ class MakeReport:
 
 		for row in range(len(content)):
 			contentrowsplit = content[row].split("\t")
+			contentrowsplit1 = content[row].split("\t")
 			# Met en gras les legendes
 			Gras=False
 			if contentrowsplit[0] == "Gene" or contentrowsplit[0] == "Echantillon":
@@ -209,40 +213,44 @@ class MakeReport:
 			for col in range(len(contentrowsplit)):
 				if len(contentrowsplit) == 1:
 					if "=" in contentrowsplit[0]:
-						font = Font(name='Arial',size=10, bold=False)
+						font = Font(name='Arial',size=12, bold=False)
 						cell = "A"+str(row+1)
 						ws[cell] = content[row]
 						ws[cell].font = font
 					else:
-						font = Font(name='Arial',size=10, bold=True, underline='single')
+						font = Font(name='Arial',size=12, bold=True, underline='single')
 						cell = "A"+str(row+1)
 						ws[cell] = content[row]
 						ws[cell].font = font
 
 				else:
 					if Gras:
-						font = Font(name='Arial',size=8, bold=True)
+						font = Font(name='Arial',size=10, bold=True)
+
 					else:
-						font = Font(name='Arial',size=8, bold=False)
+						font = Font(name='Arial',size=10, bold=False)
 					if ws.column_dimensions[alpahabet[col]].width != None:
+						listeHeight = []
 						if ws.column_dimensions[alpahabet[col]].width < len(contentrowsplit[col]):
 								if len(contentrowsplit[col])>15:
 
 									if Gras:
-										font = Font(name='Arial',size=8, bold=True)
+										font = Font(name='Arial',size=10, bold=True)
 										contentrowsplit[col]=str(contentrowsplit[col][:7])+str(contentrowsplit[col][7:]).replace(" ","\n",1)
-										ws.row_dimensions[row+1].height = 20
+										ws.row_dimensions[row+1].height = 30
 									else:
-										if len(contentrowsplit[col])<20:
-											font = Font(name='Arial',size=7, bold=False)
-										elif len(contentrowsplit[col])<25:
-											font = Font(name='Arial',size=6, bold=False)
-										else:
-											font = Font(name='Arial',size=5, bold=False)
-##########OK
-									#contentrowsplit[col]=str(contentrowsplit[col][:10])+str(contentrowsplit[col][10:]).replace(" ","\n",1)
-									#if a!=contentrowsplit[col]:
-									#	ws.row_dimensions[row].height = 50
+										font = Font(name='Arial',size=10, bold=False)
+										a=1
+										height=20
+										contentrowsplit1[col]=contentrowsplit[col]
+										for caractere in contentrowsplit[col]:
+											a+=1
+											if a%12==True:
+												contentrowsplit1[col]=contentrowsplit1[col][:a]+"\n"+contentrowsplit1[col][a:]
+												height+=20
+										listeHeight.append(height)
+										ws1.row_dimensions[row+1].height = max(listeHeight)
+										contentrowsplit[col]=contentrowsplit1[col]
 						
 						if col == len(contentrowsplit)-1:
 							cell = alpahabet[col]+str(row+1)
@@ -252,11 +260,7 @@ class MakeReport:
 							ws[cell].alignment = Alignment(horizontal="center",vertical="center")
 						else:
 							cell = alpahabet[col]+str(row+1)
-							#print(ws.column_dimensions[alpahabet[col]].width)
-							"""if ws.column_dimensions[alpahabet[col]].width< len(contentrowsplit[col]):
-								ws.column_dimensions[alpahabet[col]].width= len(contentrowsplit[col])-(25/100*len(contentrowsplit[col]))"""
 							ws[cell] = contentrowsplit[col]
-							#print(contentrowsplit[col])
 							ws[cell].font = font
 							ws[cell].border = border_thin
 							ws[cell].alignment = Alignment(horizontal="center",vertical="center")
@@ -270,22 +274,21 @@ class MakeReport:
 		# Largeur des colonnes
 		#####
 		#Pour le sinformations:
-		ws1.column_dimensions["A"].width = 10.0
+		ws1.column_dimensions["A"].width = 8.0
 		ws1.column_dimensions["B"].width =0# 10.0		
 		ws1.column_dimensions["C"].width = 10.0
 		ws1.column_dimensions["D"].width =10		
 		ws1.column_dimensions["E"].width =15# 12.0		
-		ws1.column_dimensions["F"].width =15# 10.0
+		ws1.column_dimensions["F"].width =13# 10.0
 		ws1.column_dimensions["G"].width = 0
-		ws1.column_dimensions["H"].width = 10.0
+		ws1.column_dimensions["H"].width = 8.0
 		ws1.column_dimensions["I"].width = 10
 		ws1.column_dimensions["J"].width = 0
 		ws1.column_dimensions["K"].width = 12
 		ws1.column_dimensions["L"].width =10
 		ws1.column_dimensions["M"].width =0 
-		ws1.column_dimensions["N"].width =6.0
-		ws1.column_dimensions["O"].width = 6.0
-		ws1.column_dimensions["P"].width = 6.0
+		ws1.column_dimensions["N"].width =9.0
+
 
 		
 
@@ -293,30 +296,15 @@ class MakeReport:
 		content = fichier2.readlines()
 		border_thin = Border(left=Side(style='thin',color='FF000000'),right=Side(style='thin',color='FF000000'),top=Side(style='thin',color='FF000000'),bottom=Side(style='thin',color='FF000000'))
 		alpahabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ","BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ"]
-		"""
-		column_widths = []
-		print(content)
-		for row in content:
-			for a, cell in enumerate(row):
-				if len(column_widths) > a:
-					if len(cell) > column_widths[a]:
-						column_widths[a] = len(cell)
-				else:
-					column_widths += [len(cell)]
-
-		for a, column_width in enumerate(column_widths):
-			ws.column_dimensions[get_column_letter(a+1)].width = column_width
-		"""
 		NotDef=True
 		for row in range(len(content)):
 			contentrowsplit = content[row].split("\t")
+			contentrowsplit1 = content[row].split("\t")	
 			# Met en gras les legendes
 			Gras=False
 			if contentrowsplit[0] == "Gene" or contentrowsplit[0] == "Echantillon":
 				Gras=True
-				
-			#else:
-				#font = Font(name='Arial',size=8, bold=False)
+			listeHeight = []
 			for col in range(len(contentrowsplit)):
 				if len(contentrowsplit) == 1:
 					if NotDef:
@@ -335,29 +323,31 @@ class MakeReport:
 
 				else:
 					if Gras:
-						font = Font(name='Arial',size=8, bold=True)
+						font = Font(name='Arial',size=10, bold=True)
 					else:
-						font = Font(name='Arial',size=8, bold=False)
-					if ws1.column_dimensions[alpahabet[col]].width != None:
+						font = Font(name='Arial',size=10, bold=False)
+					if ws1.column_dimensions[alpahabet[col]].width != None:						
 						if ws1.column_dimensions[alpahabet[col]].width < len(contentrowsplit[col]):
-								if len(contentrowsplit[col])>15:
-
-									if Gras:
-										font = Font(name='Arial',size=8, bold=True)
-										contentrowsplit[col]=str(contentrowsplit[col][:7])+str(contentrowsplit[col][7:]).replace(" ","\n",1)
-										ws1.row_dimensions[row+1].height = 20
-									else:
-										if len(contentrowsplit[col])<20:
-											font = Font(name='Arial',size=7, bold=False)
-										elif len(contentrowsplit[col])<25:
-											font = Font(name='Arial',size=6, bold=False)
-										else:
-											font = Font(name='Arial',size=5, bold=False)
-##########OK
-									#contentrowsplit[col]=str(contentrowsplit[col][:10])+str(contentrowsplit[col][10:]).replace(" ","\n",1)
-									#if a!=contentrowsplit[col]:
-									#	ws.row_dimensions[row].height = 50
-						
+							
+							if len(contentrowsplit[col])>15:
+								
+								if Gras:
+									font = Font(name='Arial',size=10, bold=True)
+									contentrowsplit[col]=str(contentrowsplit[col][:7])+str(contentrowsplit[col][7:]).replace(" ","\n",1)
+									ws1.row_dimensions[row+1].height = 20
+								else:
+									font = Font(name='Arial',size=10, bold=False)
+									a=1
+									height=20
+									contentrowsplit1[col]=contentrowsplit[col]
+									for caractere in contentrowsplit[col]:
+										a+=1
+										if a%12==True:
+											contentrowsplit1[col]=contentrowsplit1[col][:a]+"\n"+contentrowsplit1[col][a:]
+											height+=20
+									listeHeight.append(height)
+									ws1.row_dimensions[row+1].height = max(listeHeight)
+									contentrowsplit[col]=contentrowsplit1[col]
 						if col == len(contentrowsplit)-1:
 							cell = alpahabet[col]+str(row+1)
 							#print(cell)
@@ -366,11 +356,7 @@ class MakeReport:
 							ws1[cell].alignment = Alignment(horizontal="center",vertical="center")
 						else:
 							cell = alpahabet[col]+str(row+1)
-							#print(ws.column_dimensions[alpahabet[col]].width)
-							"""if ws.column_dimensions[alpahabet[col]].width< len(contentrowsplit[col]):
-								ws.column_dimensions[alpahabet[col]].width= len(contentrowsplit[col])-(25/100*len(contentrowsplit[col]))"""
 							ws1[cell] = contentrowsplit[col]
-							#print(contentrowsplit[col])
 							ws1[cell].font = font
 							ws1[cell].border = border_thin
 							ws1[cell].alignment = Alignment(horizontal="center",vertical="center")
